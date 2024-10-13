98b8a71e587ccf6420207f463938d5939d58079d
Rory Doak <rory@opensystemslab.io>
2024-10-13 13:48:27 +0100

    add dynamic multitext-chip and title component

---
+++ src/components/TitleBlock/TitleBlock.tsx
@@ -0,0 +1,13 @@
+import { Title } from '../shared/StyledComponents/StyledComponents'
+import './TitleBlock.scss'
+
+export default function TitleBlock() {
+
+    // title text
+    // title subtext
+    // preamble
+
+return <>
+<Title>Empty Homes</Title>
+</>
+}
---
+++ src/components/shared/StyledComponents/StyledComponents.tsx
@@ -0,0 +1,13 @@
+import { styled, Typography } from "@mui/material";
+
+export const Title = styled(Typography)(() => ({
+  color: "#242424",
+  fontSize: "6rem",
+  fontWeight: 700,
+}));
+
+export const Subtitle = styled(Typography)(() => ({
+  color: "#242424",
+  fontSize: "6rem",
+  fontWeight: 700,
+}));
