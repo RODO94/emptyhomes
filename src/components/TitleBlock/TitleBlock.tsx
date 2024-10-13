59d10175e775e62c8a0f7b5c8b49f20e9ef8a46c
Rory Doak <rory@opensystemslab.io>
2024-10-13 14:36:56 +0100

    simplified component use across breakpoints

--- src/components/TitleBlock/TitleBlock.tsx
+++ src/components/TitleBlock/TitleBlock.tsx
@@ -1,4 +1,4 @@
-import { Body, Subtitle, Title } from "../shared/StyledComponents/StyledText";
+import Typography from "@mui/material/Typography";
 import "./TitleBlock.scss";
 
 export default function TitleBlock() {
@@ -9,14 +9,16 @@ export default function TitleBlock() {
   return (
     <section className="title-block">
       <div className="title-block__box">
-        <Title>Empty Homes</Title>
-        <Subtitle>How do they affect us and our communities?</Subtitle>
+        <Typography variant="h1">Empty Homes</Typography>
+        <Typography variant="h2">
+          How do they affect us and our communities?
+        </Typography>
       </div>
-      <Body>
+      <Typography variant="body1">
         The following work has been pulled together by the Empty Homes Action
         Group to understand the affect of vacant homes on our communities and on
         the way we live our lives.{" "}
-      </Body>
+      </Typography>
     </section>
   );
 }
