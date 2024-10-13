2732809896084372cda6f41d5fad68044fc7cc57
Rory Doak <rory@opensystemslab.io>
2024-10-13 14:02:41 +0100

    add style comps for headings, body, action text, and captions

--- src/components/shared/StyledComponents/StyledComponents.tsx
+++ src/components/shared/StyledComponents/StyledComponents.tsx
@@ -8,6 +8,37 @@ export const Title = styled(Typography)(() => ({
 
 export const Subtitle = styled(Typography)(() => ({
   color: "#242424",
-  fontSize: "6rem",
-  fontWeight: 700,
+  fontSize: "1.5rem",
+  fontWeight: 400,
+}));
+
+export const Body = styled(Typography)(() => ({
+  color: "#242424",
+  fontSize: "1rem",
+  fontWeight: 300,
+}));
+
+export const Heading = styled(Typography)(() => ({
+  color: "#242424",
+  fontSize: "2rem",
+  fontWeight: 600,
+}));
+
+export const Subheading = styled(Typography)(() => ({
+  color: "#242424",
+  fontSize: "1.9rem",
+  fontWeight: 200,
+}));
+
+export const Caption = styled(Typography)(() => ({
+  color: "#242424",
+  fontSize: "0.85rem",
+  fontWeight: 300,
+}));
+
+export const ActionText = styled(Typography)(() => ({
+  color: "#10403C",
+  fontSize: "0.625rem",
+  fontWeight: 400,
+  fontVariant: "all-small-caps",
 }));
